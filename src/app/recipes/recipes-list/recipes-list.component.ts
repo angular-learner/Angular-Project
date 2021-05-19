ff427818f7b898ef8faddf2fe569feb56d340b86
Tamminedi <lathakoduri994@gmail.com>
2021-05-18 21:02:02 -0400

    adding content to recipes component

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-
+recipes : Recipe[] =[
+  new Recipe('the recipe name','the item description', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg')
+];
   constructor() { }
 
   ngOnInit(): void {
