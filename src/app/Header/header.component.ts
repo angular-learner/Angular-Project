e905d84783517cb09896eb0b9a84fc67bac9b833
unknown <lathakoduri994@gmail.com>
2022-02-03 15:18:21 -0500

    Storing Recipes

--- src/app/Header/header.component.ts
+++ src/app/Header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
     selector : 'app-header',
@@ -7,8 +8,16 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class HeaderComponent{
     @Output() onFeatureSelect  = new EventEmitter<string>();
 
+    constructor(private dataService: DataStorageService){}
+
+    onSaveData(){
+        this.dataService.storeRecipes();
+    }
+
     onSelect(feature: string){
         this.onFeatureSelect.emit(feature);
+
+        
 
     }
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,11 +1,17 @@
 import {Injectable, OnInit} from '@angular/core'
 import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService implements OnInit {
 
-    constructor(private http: HttpClient ){}
+    constructor(private http: HttpClient, private recipeService : RecipeService ){}
 ngOnInit(): void {
     
+}
+
+storeRecipes(){
+    const recipes = this.recipeService.getRecipes();
+    this.http.put('https://ng-course-recipe-book-2fe87-default-rtdb.firebaseio.com/recipes.json', recipes);
 }
 }
